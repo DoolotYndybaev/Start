5548aa7a473867b248c5acc7ccca688fc3ffbfae
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-09-23 14:26:09 +0600

    Initial Commit

---
+++ Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Start
+//
+//  Created by Doolot on 23/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
